4be0be331143bb0cf5139488121fc6c253046c9c
Marirosa <arqmvilcherrez@gmail.com>
2023-11-07 20:31:41 -0500

    Printing teachers

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -10,6 +10,27 @@ interface Teacher {
   [key: string]: any,
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+interface printTeacherFunction {
+  firstName: string, 
+  lastname: string,
+}
+
+function printTeacher (firstName: Teacher["firstName"], lastName: Teacher["lastName"]) {
+  const sliptFirstName = firstName.split('');
+  const firstElement = sliptFirstName[0];
+  return (`${firstElement}.${lastName}`)
+}
+
+/*
+//Exercise3:
+const printTeacher1 = printTeacher("John", "Doe")
+console.log(printTeacher1)
+
+//Exercise2
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -19,3 +40,13 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+Exercise1:
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);*/
