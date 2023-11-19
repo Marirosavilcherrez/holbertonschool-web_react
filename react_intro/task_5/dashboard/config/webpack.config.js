a2b45609cdfbdf3bd551ee802a79209e3a05c79c
Marirosa <arqmvilcherrez@gmail.com>
2023-11-19 01:11:12 -0500

    Create a project using Webpack

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,23 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
---
+++ react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import './style.css';
+
+function hello(name) {
+    console.log(`Hello, ${name}`)
+}
+
+hello('Holberton');
