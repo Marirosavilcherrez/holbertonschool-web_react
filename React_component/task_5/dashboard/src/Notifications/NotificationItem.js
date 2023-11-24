88528f819f11913742da5901213a6be3d327ebaf
Marirosa <arqmvilcherrez@gmail.com>
2023-11-24 14:10:03 -0500

    Declare a pure component in React

---
+++ React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,50 @@
+// Notificationitem.js
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// NotificationItem functional component that displays a notification
+//Pure component using react.memo it will only update when its props and state are different
+const NotificationItem = React.memo(({ type, html, value, markAsRead }) => {
+  let element;
+
+  // Conditional rendering based on the existence of 'value'
+  value 
+    ? (element = (
+        <li 
+          data-notification-type={type}
+          // Click handler to mark the notification as read
+          onClick={markAsRead}
+        >
+          {value}
+        </li>
+      ))
+    : (element = (
+        <li 
+          data-notification-type={type}
+          // Render HTML content dangerously (handle with care)
+          dangerouslySetInnerHTML={{__html: html}}
+          // Click handler to mark the notification as read
+          onClick={markAsRead}
+        ></li>
+      ));
+
+  return element;
+});
+
+// PropTypes for type-checking the component's props
+NotificationItem.propTypes = {
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  markAsRead: PropTypes.func.isRequired,
+};
+
+// Default props for the component
+NotificationItem.defaultProps = {
+  isLoggedIn: false,
+  type: 'default',
+}
+
+export default NotificationItem;
