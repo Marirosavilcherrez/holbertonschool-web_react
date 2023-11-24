b7b0e9c2fbc1b481390ca115b9aec92a998975ff
Marirosa <arqmvilcherrez@gmail.com>
2023-11-23 21:47:15 -0500

    Use the new components en React

---
+++ React_component/task_3/dashboard/src/App/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+];
+
+class App extends Component {
+  // Event handler for keydown event
+  handleKeyDown = (e) => {
+    if (e.ctrlKey && e.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  // Attach event listener when the component is mounted
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  // Remove event listener when the component is unmounted
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <>
+        {/* Display notifications */}
+        <Notifications listNotifications={listNotifications} />
+        {/* Display header */}
+        <Header />
+        {/* Display CourseList or Login based on isLoggedIn */}
+        {isLoggedIn ? (
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList listCourses={listCourses} />
+          </BodySectionWithMarginBottom>
+        ) : (
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login />
+          </BodySectionWithMarginBottom>
+        )}
+        {/* Display news section */}
+        <BodySection title="News from the School">
+          <p>Hi everyone!</p>
+        </BodySection>
+        {/* Display footer */}
+        <Footer />
+      </>
+    );
+  }
+}
+
+// Default props for App component
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+// PropTypes for App component
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func.isRequired,
+};
+
+export default App;
